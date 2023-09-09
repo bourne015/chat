360da33120dbc88bbdcd7205f9a649e834b8604a
fan <phantasy@qq.com>
2023-09-09 15:26:32 +0000

    enable SSE for flutter web
    EventSourceResponse works only on web
    StreamingResponse works only on mobile and desktop
    
    Signed-off-by: fan <phantasy@qq.com>

--- api/v1/text.py
+++ api/v1/text.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from sse_starlette.sse import EventSourceResponse
 from fastapi.responses import StreamingResponse
 import json
+import asyncio
 
 from utils import chat
 
@@ -17,13 +18,16 @@ class Reqdata(BaseModel):
 
 
 @router.post("/chat", name="chat")
-async def ask(question: List) -> Any:
+async def ask(data: Reqdata) -> Any:
     """
     """
     #print("chat len:", len(question))
-    print("Q:", question[-1]["content"])
+    #print("Q:", question[-1]["content"])
+    model = data.model
+    question = data.question
+    # print(f"model: {model}, Q: {question[-1]['content']}")
     try:
-        answer = chat.asks(question)
+        answer = chat.asks(question, model)
     except Exception as e:
         print("err:", e)
         return JSONResponse(status_code=500, content=str(e))
@@ -37,17 +41,18 @@ async def ask_stream(data: Reqdata) -> Any:
     """
     model = data.model
     question = data.question
-    # print("Q:", question[-1]["content"])
-    def event_generator():
+    # print(f"model: {model}, Q: {question[-1]['content']}")
+    async def event_generator():
         try:
-            answer = chat.asks(question, stream = True)
+            answer = chat.asks(question, model, stream = True)
         except Exception as err:
             yield err
         for text in answer:
             cont = text['choices'][0].get('delta' ,None).get('content', None)
             if cont:
                 yield cont
+                await asyncio.sleep(0.01)
 
-    #return EventSourceResponse(event_generator())
-    return  StreamingResponse(event_generator(), media_type="text/event-stream")
+    return EventSourceResponse(event_generator())
+    #return  StreamingResponse(event_generator(), media_type="text/event-stream")
 
