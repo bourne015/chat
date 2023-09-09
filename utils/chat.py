9fe5333c6ed86cc59b20bc3e80d92d8d86da87c4
fan <phantasy@qq.com>
2023-09-09 15:24:26 +0000

    enable gpt-4.0
    Signed-off-by: fan <phantasy@qq.com>

--- utils/chat.py
+++ utils/chat.py
@@ -8,17 +8,27 @@ class Chat:
     '''
     wrapper openai api
     '''
+    supported_models = [
+            "gpt-3.5-turbo-16k-0613",
+            "gpt-4-0613"
+    ]
+
     def __init__(self) -> None:
         #self.chat_list = []
-        self.model = "gpt-3.5-turbo-16k-0613"
+        self.model = self.supported_models[0]
         openai.api_key = settings.openai_key
         print("Chat init: ", self.model)
 
     @retry(tries=3, delay=1, backoff=1)
-    def ask(self, prompt_list, stream = False):
+    def ask(self, prompt_list, model, stream = False):
         '''
         question without context
         '''
+        if model == "gpt35":
+            self.model = self.supported_models[0]
+        elif model == 'gpt40':
+            self.model = self.supported_models[1]
+
         res = openai.ChatCompletion.create(
             model=self.model,
             messages=[{"role": "user", "content": question}],
@@ -29,11 +39,16 @@ class Chat:
         return res
 
     @retry(tries=3, delay=1, backoff=1)
-    def asks(self, prompt_list, stream = False):
+    def asks(self, prompt_list, model, stream = False):
         '''
         question with context
         prompt_list store a session of prompts and answers
         '''
+        if model == "gpt35":
+            self.model = self.supported_models[0]
+        elif model == 'gpt40':
+            self.model = self.supported_models[1]
+
         res = openai.ChatCompletion.create(
             model=self.model,
             messages=prompt_list,
