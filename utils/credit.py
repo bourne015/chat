6404d945280b8103a605f1e672d27667181aa6c3
fan <phantasy015@qq.com>
2025-01-11 16:59:25 +0000

    update model price
    Signed-off-by: fan <phantasy015@qq.com>

--- utils/credit.py
+++ utils/credit.py
@@ -13,13 +13,20 @@ class Credit:
         "gpt-4-1106-preview":   {"input": 10.0, "output": 30.0},
         "gpt-4-turbo":          {"input": 10.0, "output": 30.0},
         "gpt-4-vision-preview": {"input": 10.0, "output": 30.0},
-        "gpt-4o":               {"input": 5.0, "output": 15.0},
+        "gpt-4o":               {"input": 2.50, "output": 10.0},
         "gpt-4o-mini":          {"input": 0.15, "output": 0.6},
+        "o1":                   {"input": 15.0, "output": 60.0},
+        "o1-mini":              {"input": 3.00, "output": 12.0},
         "dall-e-3":             {"input": 10.0, "output": 30.0},
         "claude-3-haiku-20240307":  {"input": 0.25, "output": 1.25},
+        "claude-3-5-haiku-20241022":  {"input": 0.8, "output": 4.0},
         "claude-3-sonnet-20240229": {"input": 3.0, "output": 15.0},
-        "claude-3-opus-20240229":   {"input": 15.0, "output": 75.0},
         "claude-3-5-sonnet-20240620": {"input": 3.0, "output": 15.0},
+        "claude-3-5-sonnet-20241022": {"input": 3.0, "output": 15.0},
+        "claude-3-opus-20240229":     {"input": 15.0, "output": 75.0},
+        "deepseek-chat":              {"input": 0.10, "output": 2.00}, # not dollar
+        "gemini-1.5-pro":             {"input": 0.10, "output": 0.10}, # free
+        "gemini-2.0-flash-exp":       {"input": 0.10, "output": 0.10}, # free
     }
     defalt_price = {"input": 10.0, "output": 30.0}
 
